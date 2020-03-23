898c084910c47a4505524051ac101e84f4a8da6a
Dranikf <kobfedsur@gmail.com>
2020-03-23 13:46:18 +0300

    задание 3 в домашке начал

--- homework/hw1/hw1t2.m
+++ homework/hw1/hw1t2.m
@@ -1,14 +1,9 @@
-function hw1t2(m , b)
+function [mSol , sysSol] = hw1t2(m , b)
 	
-	disp('solution by matrix variant');
-	inv(m) * b
+	b = reshape(b , numel(b) , 1);
+	mSol = inv(m) * b;
 	
-	disp('solution by solve function')
-	syms x;
-	s = size(m);
-	for (i = 1:s(1))
-		for (j = 1:s(2))
-		end
-	end
+	linSystem = linSysByMat(m , b);	
+	sysSol = solve(linSystem);
 
 end
---
+++ homework/hw1/hw1t3.m
@@ -0,0 +1,6 @@
+function [matSol , sysSol] hw1t3(m , b)
+
+	b = reshape(b , numel(b) , 1)
+	mSol =  
+	
+end
